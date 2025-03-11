2511b40189c1de3d37d18508284e79bf8210b742
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2025-03-11 12:29:55 +0200

    Bump version

--- Umuriro/Wumuriro Watch App/WumuriroApp.swift
+++ Umuriro/Wumuriro Watch App/WumuriroApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Wumuriro_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            CalculatorView()
+            ConverterView()
         }
     }
 }
